3a6272d8e9ce79f2516455356d1837d106519db0
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-22 13:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+const outputDiv=document.getElementById("output");
+const input=document.getElementById("text");
+const number=document.getElementById("delay");
+const button=document.getElementById("btn");
+
+button.addEventListener("click",function(){
+	
+const inputValue=input.value;
+const numberValue=number.value;
+	setTimeout(()=>{
+		outputDiv.innerHTML=inputValue;
+	},numberValue)
+})
